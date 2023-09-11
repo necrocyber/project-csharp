e6a52decde750d083bdd636d31e7440cf2959f76
necrocyber <amedel.zen@gmail.com>
2023-09-11 19:38:02 -0300

    change program

--- jorge-medel/Program.cs
+++ jorge-medel/Program.cs
@@ -6,8 +6,6 @@ namespace FinalProject
     {
         static void Main(string[] args)
         {
-            Usuario usuario = new Usuario(1, "Jorge", "Medel", "Joker", "1245kiol", "amedel.zen@gmail.com");
-            
             Console.WriteLine("Final Project!");
             Console.ReadKey();
         }
